b673c85b070a4ebdea34056d0ba130f47924bc68
thilipython <karoltharaka@gmail.com>
2024-07-01 00:38:48 +0530

    Initial commit

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EmployeeCRUDApplication.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EmployeeCRUDApplication.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
---
+++ Models/AddEmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EmployeeCRUDApplication.Models
+{
+	public class AddEmployeeViewModel
+	{
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public long Salary { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Department { get; set; }
+    }
+}
+
